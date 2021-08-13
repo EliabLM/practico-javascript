d80097caae918e782dd4e2e333217f7dfa23d433
eliablm <eliablopez@hotmail.com>
2021-08-13 17:26:49 -0500

    modificar header y agregar responsive mobile figuras geometricas

--- figuras/figuras.js
+++ figuras/figuras.js
@@ -1,3 +1,11 @@
+// Mobile
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function () {
+	links.classList.toggle('show-links');
+});
+
 // Cuadrado
 
 function perimetroCuadrado(lado) {
